1b35e4f7fd1dd630c1d7674c3711e07fbab87597
Christian Andersen <christian@milindur.de>
2015-05-16 10:45:22 +0200

    Umbenennung

--- update/flash.bat
+++ update/flash.bat
@@ -4,4 +4,4 @@ PORT = COM5
 
 mode %PORT%:1200 > nul
 timeout /T 1 > nul
-bossac --port=%PORT% -U false -e -w -v -b SliderCtrlBle.bin -R
+bossac --port=%PORT% -U false -e -w -v -b MdkController.bin -R
