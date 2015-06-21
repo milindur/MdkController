aa30c69a31d1ae10b09e9b05c2fdc82abd5bd1e8
Christian Andersen <christian@milindur.de>
2015-06-21 13:26:18 +0100

    Version v4

--- MdkController/src/version.h
+++ MdkController/src/version.h
@@ -2,8 +2,8 @@
 #define _VERSION_H_
 
 #define versionDEVICE_NAME_DEFAULT		"MDK Pan/Tilt"
-#define versionFIRMWARE_VERSION_STRING	"v3"
-#define versionFIRMWARE_VERSION			3
+#define versionFIRMWARE_VERSION_STRING	"v4"
+#define versionFIRMWARE_VERSION			4
 
 extern const char VERSION_VERSION[];
 extern const char VERSION_COMPILE_TIME[];
