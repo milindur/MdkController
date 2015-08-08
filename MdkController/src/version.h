1763ab2717638ba5c99f02ecf0c4913f399edef5
Christian Andersen <christian@milindur.de>
2015-08-08 22:32:58 +0200

    Erhöhung der Version auf v8

--- MdkController/src/version.h
+++ MdkController/src/version.h
@@ -1,8 +1,8 @@
 #ifndef _VERSION_H_
 #define _VERSION_H_
 
-#define versionDEVICE_NAME_DEFAULT		"MDK P/T v07"
-#define versionFIRMWARE_VERSION_STRING	"v7"
+#define versionDEVICE_NAME_DEFAULT		"MDK P/T v08"
+#define versionFIRMWARE_VERSION_STRING	"v8"
 #define versionFIRMWARE_VERSION			40
 
 extern const char VERSION_VERSION[];
