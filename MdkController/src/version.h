a0861088319bf876380aeedd59fd986eb3ea6c4c
Christian Andersen <christian@milindur.de>
2015-09-26 21:56:55 +0200

    Versionsnr. aktualisiert

--- MdkController/src/version.h
+++ MdkController/src/version.h
@@ -1,8 +1,8 @@
 #ifndef _VERSION_H_
 #define _VERSION_H_
 
-#define versionDEVICE_NAME_DEFAULT		"MDK P/T v09"
-#define versionFIRMWARE_VERSION_STRING	"v9"
+#define versionDEVICE_NAME_DEFAULT		"MDK P/T v10"
+#define versionFIRMWARE_VERSION_STRING	"v10"
 #define versionFIRMWARE_VERSION			40
 
 extern const char VERSION_VERSION[];
