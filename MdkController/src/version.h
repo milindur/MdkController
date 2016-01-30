f191e27f45c3d2cf1e841fb4588eb3833414302f
Christian Andersen <christian@milindur.de>
2016-01-30 21:35:12 +0100

    Bumped version string

--- MdkController/src/version.h
+++ MdkController/src/version.h
@@ -1,8 +1,8 @@
 #ifndef _VERSION_H_
 #define _VERSION_H_
 
-#define versionDEVICE_NAME_DEFAULT		"MDK P/T v11"
-#define versionFIRMWARE_VERSION_STRING	"v11"
+#define versionDEVICE_NAME_DEFAULT		"MDK P/T v12"
+#define versionFIRMWARE_VERSION_STRING	"v12"
 #define versionFIRMWARE_VERSION			44
 
 extern const char VERSION_VERSION[];
